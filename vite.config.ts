5ece56f027bca434c9400958dd55e9a220bab1ae
j71951804-blip <j71951804@gmail.com>
2025-08-03 07:20:56 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': new URL('./src', import.meta.url).pathname
+      '@': new URL('./', import.meta.url).pathname
     }
   },
   server: {
