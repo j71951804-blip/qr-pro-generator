67fa3c47393d6b05a3f363e411806d882038a891
j71951804-blip <j71951804@gmail.com>
2025-08-05 20:03:44 +0100

    Update serviceWorker.ts

--- utils/serviceWorker.ts
+++ utils/serviceWorker.ts
@@ -1,6 +1,6 @@
 // utils/serviceWorker.ts
 export const registerSW = () => {
-  if ('serviceWorker' in navigator && import.meta.env.PROD) {
+  if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/sw.js')
         .then((registration) => {
