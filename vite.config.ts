36259858b5b9fbe4bb4b9cc26c821fbce1f03380
j71951804-blip <j71951804@gmail.com>
2025-08-07 12:25:14 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import prerender from 'vite-plugin-prerender'
+import prerender from 'vite-prerender-plugin'
+
 import { resolve } from 'path'
 
 export default defineConfig({
